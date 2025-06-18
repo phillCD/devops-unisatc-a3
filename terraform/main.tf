979178f54f60d724dea29b3795d1f44c11f471fa
GabrielGuinzani <gferreiraguinzani@gmail.com>
2025-06-17 23:28:37 -0300

    [fix] workflow changes - sucesso

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,7 +31,7 @@ resource "aws_ecs_task_definition" "strapi" {
 }
 
 resource "aws_ecs_service" "strapi" {
-  name            = "RolePolicySTRAPI"
+  name            = "strapi-service"
   cluster         = aws_ecs_cluster.strapi.id
   task_definition = aws_ecs_task_definition.strapi.arn
   desired_count   = 1
